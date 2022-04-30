aef27d50287c15bced8f903791cd67c6c4715349
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2022-04-30 13:46:55 +0000

    [finish]

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -3,7 +3,7 @@ class BookComment < ApplicationRecord
     belongs_to :user
     belongs_to :book
     
-    validates :comments, presence: true
+    validates :comment, presence: true
     
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    
+    
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-    
-    resource :book_comments, only: [:create, :destroy]
-    
   end
     
   resources :users, only: [:index,:show,:edit,:update]
